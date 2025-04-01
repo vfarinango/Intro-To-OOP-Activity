70d94c08a55b7a90397c1fb497a254402a8962d2
daniellew712 <xiaoyuew66@gmail.com>
2025-04-01 11:01:09 -0700

    updates to the comparison file

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,1 +1,12 @@
 # add your get_student_with_more_classes function here!
+
+def get_student_with_more_classes(Student1, Student2):
+
+    if Student1.get_num_classes() > Student2.get_num_classes(): 
+        return Student1.name
+
+    else:
+        return Student2.name
+
+
+
